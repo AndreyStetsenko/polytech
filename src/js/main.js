89684aa06fee80b64c9591cdfdb42c552fa72c59
Andrew Stetsenko <andrey@stetsenko.org>
2020-09-29 18:02:36 +0400

    Upload 29.09.2020 18:00

--- src/js/main.js
+++ src/js/main.js
@@ -13,3 +13,16 @@ $('.dropdown-menu a.dropdown-toggle').on('click', function(e) {
 
   return false;
 });
+
+
+$("[data-trigger]").on("click", function(){
+    var trigger_id =  $(this).attr('data-trigger');
+    $(trigger_id).toggleClass("show");
+    $('body').toggleClass("offcanvas-active");
+});
+
+// close button
+$(".btn-close").click(function(e){
+    $(".navbar-collapse").removeClass("show");
+    $("body").removeClass("offcanvas-active");
+});
